cdfcecd25dbb9fd2b33292abeca5f5c92776dbf9
Lehi Toskin <lehi@tosk.in>
2017-01-20 16:40:08 -0800

    draw-lib probably isn't necessary

--- info.rkt
+++ info.rkt
@@ -5,8 +5,12 @@
 
 (define blurb '("Riff is a wrapper for the FLIF library."))
 (define primary-file "main.rkt")
+(define homepage "https://github.com/lehitoskin/riff/")
+
+(define version "0.1")
+(define release-notes '("Make interface a little more racket-y."))
 
 (define required-core-version "6.1")
 
-(define deps '("base" "draw-lib" "scribble-lib"))
+(define deps '("base" "scribble-lib"))
 (define build-deps '("racket-doc"))
